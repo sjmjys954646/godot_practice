6b8debd04a34362632ed79d1bd706e7e894c6106
sjmjys954646 <sjmjys954646@gmail.com>
2024-11-09 10:25:04 +0900

    feat : deploy

--- src/game_over.gd
+++ src/game_over.gd
@@ -9,7 +9,3 @@ func _ready() :
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("shoot") :
 		get_tree().change_scene_to_packed(level_scene)
-
-func _input(event) :
-	if event.is_action_just_pressed("shoot") :
-		get_tree().change_scene_to_packed(level_scene)
